c97539e287039c9be63cf320541b45c1e542c5b9
Aarushi Singh <165064252+aarushixsingh@users.noreply.github.com>
2025-04-21 20:32:19 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,46 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import ScanPage from "./pages/ScanPage";
+import ThreatsPage from "./pages/ThreatsPage";
+import SettingsPage from "./pages/SettingsPage";
+import NotFound from "./pages/NotFound";
+import Layout from "./components/layout/Layout";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/scan" element={
+            <Layout>
+              <ScanPage />
+            </Layout>
+          } />
+          <Route path="/threats" element={
+            <Layout>
+              <ThreatsPage />
+            </Layout>
+          } />
+          <Route path="/settings" element={
+            <Layout>
+              <SettingsPage />
+            </Layout>
+          } />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
